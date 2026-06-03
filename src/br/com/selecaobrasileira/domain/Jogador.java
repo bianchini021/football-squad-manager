6e795a65edfe9b85b5c88da4070552ea8abd8cb2
Eduardo Bianchini Borges <eduardo.borges@sedes.es.gov.br.local>
2026-06-03 14:39:06 -0300

    Correção classe Jogador

--- src/br/com/selecaobrasileira/domain/Jogador.java
+++ src/br/com/selecaobrasileira/domain/Jogador.java
@@ -1,6 +1,7 @@
 package br.com.selecaobrasileira.domain;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Jogador {
     private int Id;
@@ -10,4 +11,74 @@ public class Jogador {
     private int numCamisa;
     private Posicao posicao;
     private boolean convocado;
+
+    public Jogador(int id, String nome, LocalDate dataNascimento, String clube, int numCamisa, Posicao posicao, boolean convocado) {
+        Id = id;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.clube = clube;
+        this.numCamisa = numCamisa;
+        this.posicao = posicao;
+        this.convocado = convocado;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public String getClube() {
+        return clube;
+    }
+
+    public int getNumCamisa() {
+        return numCamisa;
+    }
+
+    public Posicao getPosicao() {
+        return posicao;
+    }
+
+    public boolean isConvocado() {
+        return convocado;
+    }
+
+    public void setClube(String clube) {
+        this.clube = clube;
+    }
+
+    public void setNumCamisa(int numCamisa) {
+        this.numCamisa = numCamisa;
+    }
+
+    public void setConvocado(boolean convocado) {
+        this.convocado = convocado;
+    }
+
+    public int calcularIdade(){
+        LocalDate hoje = LocalDate.now();
+        Period periodo = Period.between(dataNascimento, hoje);
+
+        return periodo.getYears();
+    }
+
+    @Override
+    public String toString() {
+        return "Jogador{" +
+                "Id=" + Id +
+                ", nome='" + nome + '\'' +
+                ", dataNascimento=" + dataNascimento +
+                ", clube='" + clube + '\'' +
+                ", numCamisa=" + numCamisa +
+                ", posicao=" + posicao +
+                ", convocado=" + convocado +
+                '}';
+    }
 }
