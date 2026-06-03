6c6e33f067768fc739ab73be13e32ec4d3338c82
Eduardo Bianchini Borges <eduardo.borges@sedes.es.gov.br.local>
2026-06-03 15:38:17 -0300

    Método cadastrarTecnico criado!

--- src/br/com/selecaobrasileira/application/Main.java
+++ src/br/com/selecaobrasileira/application/Main.java
@@ -2,6 +2,8 @@ package br.com.selecaobrasileira.application;
 
 import br.com.selecaobrasileira.domain.Jogador;
 import br.com.selecaobrasileira.domain.Posicao;
+import br.com.selecaobrasileira.domain.Tecnico;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -9,6 +11,7 @@ import java.util.Scanner;
 public class Main {
     static Scanner input = new Scanner(System.in);
     static ArrayList<Jogador> jogadoresCadastrados = new ArrayList<>();
+    static Tecnico tecnicoAtual;
     public static void main(String[] args) {
         byte opc = menuPrincipal();
         while (opc != 4){
@@ -17,6 +20,7 @@ public class Main {
                     cadastrarJogador();
                     break;
                 case 2:
+                    tecnicoAtual = cadastrarTecnico();
                     break;
                 case 3:
                     break;
@@ -78,5 +82,41 @@ public class Main {
         } else {
             System.out.println("Aconteceu um erro ao cadastrar jogador.");
         }
+    }
+
+    public static Tecnico cadastrarTecnico(){
+        if(tecnicoAtual != null){
+            System.out.println("Já existe um técnico cadastrado, tem certeza que deseja mudar? (S/N");
+            char opc = Character.toUpperCase(input.next().charAt(0));
+
+            if(opc == 'N'){
+                return tecnicoAtual;
+            } else if (opc != 'S'){
+                System.out.println("Opção inválida!");
+                return tecnicoAtual;
+            }
+        }
+        System.out.println("+--- CADASTRAR TÉCNICO ---+");
+        System.out.println("Digite o nome do técnico: ");
+        String nome = input.nextLine().toUpperCase();
+
+        System.out.println("Digite a nacionalidade do técnico: ");
+        String nacionalidade = input.nextLine();
+
+        System.out.println("Digite o ano do inicio do mandato: ");
+        int ano = Integer.parseInt(input.nextLine());
+
+        System.out.println("Digite o mês (1 - 12): ");
+        int mes = Integer.parseInt(input.nextLine());
+
+        System.out.println("Digite o dia: ");
+        int dia = Integer.parseInt(input.nextLine());
+
+        LocalDate dataInicioMandato = LocalDate.of(ano, mes,dia);
+
+        System.out.println("Técnico criado com sucesso!");
+
+
+        return new Tecnico(nome, nacionalidade, dataInicioMandato);
     }
 }
